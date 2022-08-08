20240932a61d94ddbd4202796aab8ae92f549946
James March <54935498+Fokkos@users.noreply.github.com>
2022-08-08 10:50:30 +0100

    test webhook

--- Space-Race/src/spacerace/SpaceRace.java
+++ Space-Race/src/spacerace/SpaceRace.java
@@ -3,7 +3,7 @@ package spacerace;
 public class SpaceRace {
 	
 	public static void main(String[] args) {
-		System.out.println("Testing. 1 2 3");
+		System.out.println("Testing. 1 2 3 4");
 	}
 
 }
