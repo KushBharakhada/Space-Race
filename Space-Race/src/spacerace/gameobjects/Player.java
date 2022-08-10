8cc2daa292bc10f04f43b4cd4387199b230919e7
James March <54935498+Fokkos@users.noreply.github.com>
2022-08-10 13:41:09 +0100

    WASD support

--- Space-Race/src/spacerace/gameobjects/Player.java
+++ Space-Race/src/spacerace/gameobjects/Player.java
@@ -15,10 +15,10 @@ import spacerace.gui.GUIFrame;
 
 //TODO make player movement smooth (use timer for KeyPressed and KeyReleased)
 //TODO add diagonal movement (8 directions is all u can get with keyboard)
-//TODO add borders to player movement (can't go off screen)
 //TODO Implement Lives
 //TODO Implement collision detection
 //TODO implement game over when you run out of lives
+//TODO Define colour in paintComponent
 
 public class Player {
 	
@@ -28,31 +28,39 @@ public class Player {
 	int height = 20;
 	Color color = Color.red;
 	
-	final double SPEED = 5; //how fast player should move
+	final int SPEED = 5; //how fast player should move
 	
-	//constructor, params are coords of where to spawn player
+	//constructor, parameters are coordinates of where to spawn player
 	public Player(int x, int y){
 		this.x = x;
 		this.y = y;
 	}
 	
-	
 	//borders are a bit weird because the player appears to be drawn at a strange position (not border)
 	public void keyPressed(KeyEvent e) {
-		if (e.getKeyCode() == KeyEvent.VK_UP) {
+		//up
+		if (e.getKeyCode() == KeyEvent.VK_UP || e.getKeyCode() == KeyEvent.VK_W) {
 			this.y = (y > 0) ? this.y -= SPEED : this.y;
 		}
-		if (e.getKeyCode() == KeyEvent.VK_DOWN) {
+		//down
+		if (e.getKeyCode() == KeyEvent.VK_DOWN || e.getKeyCode() == KeyEvent.VK_S) {
 			this.y = (y < GUIFrame.GAME_HEIGHT - this.height*3) 
 					? this.y += SPEED : this.y;
 		}
-		if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+		//left
+		if (e.getKeyCode() == KeyEvent.VK_LEFT || e.getKeyCode() == KeyEvent.VK_A) {
 			this.x = (x > 0) ? this.x -= SPEED : this.x;
 		}
-		if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+		//right
+		if (e.getKeyCode() == KeyEvent.VK_RIGHT || e.getKeyCode() == KeyEvent.VK_D) {
 			this.x = (x < GUIFrame.GAME_WIDTH - this.width*1.8) 
 				? this.x += SPEED : this.x;
 		}
+	}
+	
+	//runs Pythagorean theorem to calculate speed when going diagonally
+	public int calculateDiagonal(int speed) { 
+		return (int)Math.floor(Math.sqrt(2 * (speed ^ 2)));
 	}
 	
 	public void draw(Graphics g) {
