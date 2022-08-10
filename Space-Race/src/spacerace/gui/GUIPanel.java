097be1f7025a7af8ca31c433b89dc9f0c2303e29
Kush <97338412+KushBharakhada@users.noreply.github.com>
2022-08-10 11:54:16 +0100

    Controlling access during multithreading to asteroids arrayList.

--- Space-Race/src/spacerace/gui/GUIPanel.java
+++ Space-Race/src/spacerace/gui/GUIPanel.java
@@ -4,6 +4,7 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.util.ArrayList;
+import java.util.Collections;
 
 import javax.swing.JPanel;
 import spacerace.gameobjects.Asteroid;
@@ -17,7 +18,6 @@ import spacerace.gameobjects.AsteroidRunnable;
  * @author Kush Bharakhada and James March
  */
 
-// TODO Causes ConcurrentModificationException ***********
 // TODO Arraylist grows too large, dead thread asteroids need removing
 
 public class GUIPanel extends JPanel {
@@ -25,7 +25,7 @@ public class GUIPanel extends JPanel {
 	private static final long serialVersionUID = 1L;
 	
 	// Instance Variables
-	private ArrayList<Asteroid> asteroids = new ArrayList<>();
+	private ArrayList<Asteroid> asteroids = new ArrayList<>();	
 
 	public GUIPanel() {
 		this.setBackground(Color.BLACK);
@@ -35,22 +35,27 @@ public class GUIPanel extends JPanel {
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
 		Graphics2D g2d = (Graphics2D) g;
-		// Goes through all the asteroids that have been added
-		for (Asteroid a : asteroids) {
-			g2d.setColor(Color.GRAY);
-			g2d.fill(a.drawAsteroid());
+		
+		// Control access of multiple threads to the array list
+		synchronized(asteroids) {
+			// Goes through all the asteroids that have been added
+			for (Asteroid a : asteroids) {
+				g2d.setColor(Color.GRAY);
+				g2d.fill(a.drawAsteroid());
+			}
 		}
-	}
-	
-	public void add(Asteroid a) {
-		// Adds an asteroid to the asteroid array list
-		asteroids.add(a);
 	}
 	
 	public void launchAsteroid(int speed) {
 		// Creating an asteroid object
 		Asteroid a = new Asteroid(speed);
-        asteroids.add(a);
+		
+		// Reliable communication between threads
+		// Control access of multiple threads to the array list
+		synchronized(asteroids) {
+			asteroids.add(a);
+		}
+		
 		// Starts a new thread for each new asteroid
 	    Runnable r = new AsteroidRunnable(a, this);
 	    Thread t = new Thread(r);
@@ -67,31 +72,9 @@ public class GUIPanel extends JPanel {
 				e.printStackTrace();
 			}
 		    System.out.println(Thread.activeCount());
-		    System.out.println(asteroids.size());
+		    //System.out.println(asteroids.size());
 		}
 	}
 	
-	
-	// Testing Threads
-	/*
-	public Thread getThread(String name) {
-		for (Thread t : Thread.getAllStackTraces().keySet()) {
-			if (t.getName().equals(name)) {
-				return t;
-			}
-		}
-		return null;
-	}
-	
-	public void launchGame() {
-	    addAsteroid(1);
-	    launchAsteroid();
-	    System.out.println("Number of active threads: " + Thread.activeCount());
-	    while (getThread("hello") != null) {
-	    	System.out.println(getThread("hello").isAlive());
-	    }
-	    System.out.println("Number of active threads: " + Thread.activeCount());  
-	}
-	*/
 		
 }
